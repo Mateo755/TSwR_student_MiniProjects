19a09b5a8bbc24d44333a2990745445f23147a69
Mateo755 <mateusz.polska07@gmail.com>
2025-03-21 23:57:51 +0100

    Task 1 finished
    Polynomial trajectory generator implemented and some corrections in FLC class

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -7,26 +7,28 @@ class FeedbackLinearizationController(Controller):
     def __init__(self, Tp):
         self.model = ManiuplatorModel(Tp)
 
+        self.Kp = np.diag([25, 30])
+        self.Kd = np.diag([30, 40])
+
+
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Implementacja feedback linearization: wyznacz momenty sił τ, aby układ zachowywał się jak układ liniowy.
         """
-        print(q_r_dot)
+        isfeedbackPD = True
+
+
         q = x[:2]  # Aktualna pozycja (q1, q2)
         q_dot = x[2:]  # Aktualna prędkość (q̇1, q̇2)
 
-        # Obliczenie macierzy M, C na podstawie aktualnego stanu
-        M = self.model.M(x)
-        C = self.model.C(x)
-
-        # Współczynniki PD (dostosuj je!)
-        #Kp = np.diag([0.4, 0.4])  # Przykładowe wartości dla PD
-        #Kd = np.diag([0.1, 0.1])
 
         # Definicja sygnału sterującego v z PD
-        #v = q_r_ddot + Kd @ (q_dot - q_r_dot) + Kp @ (q - q_r)
-        v = q_r_ddot
+        if isfeedbackPD:
+            v = q_r_ddot + self.Kd @ (q_r_dot - q_dot) + self.Kp @ (q_r - q)
+        else:
+            v = q_r_ddot
+
         # Wyznaczenie momentów sił τ
-        tau = M @ v + C @ q_dot
+        tau = self.model.M(x) @ v + self.model.C(x) @ q_dot
 
         return tau
