18777567b9e6e9f03eae17557fa6e14d8faf4c22
Mateo755 <mateusz.polska07@gmail.com>
2025-03-27 19:12:35 +0100

    Added state matrixes to adrc_joint_cont file

--- controllers/adrc_joint_controller.py
+++ controllers/adrc_joint_controller.py
@@ -9,15 +9,39 @@ class ADRCJointController(Controller):
         self.kp = kp
         self.kd = kd
 
-        A = None
-        B = None
-        L = None
-        W = None
+        A = np.array([
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 0, 0]
+        ])
+
+        B = np.array([
+            [0],
+            [b],
+            [0]
+        ])
+
+        L = np.array([
+            [3 * p],
+            [3 * p**2],
+            [p**3]
+        ])
+
+        W = np.array([[1, 0, 0]])
+
         self.eso = ESO(A, B, W, L, q0, Tp)
 
     def set_b(self, b):
         ### TODO update self.b and B in ESO
-        return NotImplementedError
+        self.b = b
+        B = np.array([
+            [0],
+            [b],
+            [0]
+        ])
+
+        self.eso.set_B(B)
+
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
         ### TODO implement ADRC
